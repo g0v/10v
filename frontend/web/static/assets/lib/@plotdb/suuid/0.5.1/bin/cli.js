d5163301deaab6abcd4a06fd4b0ad4fdfa5557cd
zbryikt <tkirby@gmail.com>
2022-07-09 14:16:46 +0800

    remove assets files

--- frontend/web/static/assets/lib/@plotdb/suuid/0.5.1/bin/cli.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-(function(){
-  var suuid;
-  suuid = require("../dist/index");
-  console.log(suuid());
-}).call(this);
--- frontend/web/static/assets/lib/curegex/0.0.4/index.js
+++
@@ -1,4 +0,0 @@
-var generic, tw;
-generic = require("./curegex.js");
-tw = require("./curegex.tw.js");
-module.exports = (generic.tw = tw, generic);
